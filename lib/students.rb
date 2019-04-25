bd90af0f7f8ccb17cdc372218cca7343376bc806
Angelina Montanez <angelinamontanez@Angelinas-MacBook-Air.local>
2019-04-25 12:41:18 -0400

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,26 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  INSERT INTO highest_student_gpa (id, name, grade, gpa, tardies)
+  VALUEs (3, 'Zak', 12, 4, 0):
 end
 
 def lowest_student_gpa
+INSERT INTO lowest_student_gpa (id, name, grade, gpa, tardies)
+VALUEs (5, 'Bob', 10, 1.9, 17):
 end
 
 def average_student_gpa
+INSERT INTO average_student_gpa (id, name, grade, gpa, tardies)
+VALUEs (1, 'John', 11, 3.4, 3):
 end
 
 def total_tardies_for_all_students
+INSERT INTO total_tardies_for_all_students (tardies)
+VALUEs (49):
 end
 
 def average_gpa_for_9th_grade
+INSERT INTO average_gpa_for_9th_grade (gpa)
+VALUEs (2.9)
 end
